3b9d36755fd7e1e4405b130d84a89a04c00d6f82
Fuady Rosma Hidayat <fuady@live.com>
2022-02-20 15:47:42 +0700

    Reset to demo-ready condition

--- src/Calcool6.Application/SimpleCalculation.cs
+++ src/Calcool6.Application/SimpleCalculation.cs
@@ -4,7 +4,7 @@ public static class SimpleCalculation
 {
     public static int Add(int input1, int input2)
     {
-        return input1 + input2;
+        return 0;
     }
 
     public static int Subtract(int input1, int input2)
