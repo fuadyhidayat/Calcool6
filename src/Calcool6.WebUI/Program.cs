54ed02281db316b45d81d7cfb8fffc6714508df0
Fuady Rosma Hidayat <fuady@live.com>
2022-02-24 11:48:25 +0700

    Menggunakan StaticWebAssetsLoader

--- src/Calcool6.WebUI/Program.cs
+++ src/Calcool6.WebUI/Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices();
+
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
 
 var app = builder.Build();
 
